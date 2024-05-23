ba0d6f851c05b9dfcc5edcc851dcae1fa366c4d3
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2024-05-23 22:50:24 +0400

    The add function

--- Project1/Graph.cpp
+++ Project1/Graph.cpp
@@ -27,4 +27,70 @@ public:
 		}
 
 	};
+
+
+	map<Vertex, vector<pair<Dist, Vertex>>> graph;
+	set<Edge> edgeset;
+	multiset < pair<Vertex, Vertex>> checkedge;
+
+	bool has_vertex(const Vertex& v)const
+	{
+		return graph.find(v) != graph.end();
+	}
+
+	void add_vertex(const Vertex& v)
+	{
+		graph[v];
+	}
+	bool remove_vertex(const Vertex& v)
+	{
+		if (!has_vertex(v))
+			return false;
+		graph.erase(v);
+
+		for (auto c : graph)
+		{
+
+			for (int i = 0; i < c.second.size(); i++)
+			{
+				if (c.second[i].second == v)
+				{
+					graph[c.first].erase(c.second.begin() + i);
+					Edge ed(c.first, c.second[i].second, c.second[i].first);
+					edgeset.erase(ed);
+					checkedge.erase(checkedge.find({ ed.from, ed.to }));
+					i--;
+
+				}
+			}
+
+		}
+		return true;
+
+	}
+	vector<Vertex> vertices()const
+	{
+		vector<Vertex> a;
+		for (auto c : graph)
+			a.push_back(c.first);
+		return a;
+	}
+
+	void add_edge(const Vertex& from, const Vertex& to, const Dist& d)
+	{
+		if (!has_vertex(from) || !has_vertex(to))
+			return;
+		graph[from].push_back({ d, to });
+		Edge ed(from, to, d);
+		edgeset.insert(ed);
+		checkedge.insert({ ed.from, ed.to });
+	}
+	void add_edge(const Edge& ed)
+	{
+		if (!has_vertex(ed.from) || !has_vertex(ed.to))
+			return;
+		graph[ed.from].push_back({ ed.dist, ed.to });
+		edgeset.insert(ed);
+		checkedge.insert({ ed.from, ed.to });
+	}
 };
